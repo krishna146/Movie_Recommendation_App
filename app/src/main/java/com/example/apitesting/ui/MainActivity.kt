1bd1ceeba5ec3e1f9e76603be5b540a4ddd14902
Alok Deep <alokdeep1172001@gmail.com>
2023-08-06 15:30:04 +0530

    ui updated

--- app/src/main/java/com/example/apitesting/ui/MainActivity.kt
+++ app/src/main/java/com/example/apitesting/ui/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.apitesting.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.apitesting.R
 import com.example.apitesting.adapter.MovieRecyclerAdapter
 import com.example.apitesting.application.MyApplication
 import com.example.apitesting.databinding.ActivityMainBinding
 import com.example.apitesting.model.Movie
-import com.example.apitesting.model.MovieData
 import com.example.apitesting.viewmodel.MainViewModel
 import com.example.apitesting.viewmodel.MainViewModelFactory
 import kotlinx.coroutines.GlobalScope
@@ -28,10 +26,11 @@ class MainActivity : AppCompatActivity() {
         //view banding
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+
         movieAdapter = MovieRecyclerAdapter(this)
-        activityMainBinding.recyclerMovie.layoutManager =
-            LinearLayoutManager(this)
+        activityMainBinding.recyclerMovie.layoutManager = LinearLayoutManager(this)
         activityMainBinding.recyclerMovie.adapter = movieAdapter
+
         //initializing repository class using our application class
         val myRepository = (application as MyApplication).recommendRepository
         //initializing our view model using view model factory
@@ -41,10 +40,7 @@ class MainActivity : AppCompatActivity() {
                 MainViewModelFactory(myRepository)
             )[MainViewModel::class.java]
         GlobalScope.launch {
-
             mainViewModel.getMovies()
-
-
         }
 
         mainViewModel.movieResponse.observe(this, Observer {
